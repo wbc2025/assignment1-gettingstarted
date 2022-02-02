f4b12146d35cf7683b7c6563fceeca09fb41b042
W. Brett Christiansen <wbchristiansen@gmail.com>
2022-02-01 22:44:36 -0600

    fix DHCP question

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ answers = {
     "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
     "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
     "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-    "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 3,
+    "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 5,
     "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": 4
 }
 
